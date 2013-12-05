b9d459646ee124f40d910f813109b872571d1e4a
Sadhana Sharma <sadhana_sharma_engg@yahoo.com>
2013-12-05 18:53:53 +0530

    Added password and encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-
+ attr_accessor :password
+	
 	email_regex =  /\A[\w._-]+@([\da-z]*\.)+[\da-z]+\z/i
 
 	validates  :name ,  :presence => true,
@@ -19,5 +21,11 @@ class User < ActiveRecord::Base
 	validates  :email , :presence => true,
 	:format => {:with => email_regex},
 	:uniqueness => {:case_sensitive => false}
+
+	validates :password , :presence => true,
+				:confirmation => true,
+				:length => { :within => 6..40 }
+
+	validates_confirmation_of :password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204173149) do
+ActiveRecord::Schema.define(version: 20131205131927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20131204173149) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
